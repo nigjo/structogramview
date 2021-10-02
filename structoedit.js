2d6e627b9ea9f4f1a96f7d0f2edabdb4ec88edc9
Jens Hofschröer <github@nigjo.de>
2021-10-02 15:36:44 +0200

    added 'Syntax Highlighting'

--- structoedit.js
+++ structoedit.js
@@ -38,6 +38,9 @@ function updateView(structview){
   var codeid = structview.dataset.structcodeId;
   var sourcecode = document.querySelector('#'+codeid);
   var erroritem = sourcecode.parentNode;
+  if(sourcecode.dataset.erroritem){
+    erroritem = document.getElementById(sourcecode.dataset.erroritem);
+  }
   try{
     erroritem.dataset.parsererror = '';
     var diagram = generateDiagram(structview);
@@ -54,14 +57,65 @@ function updateView(structview){
       erroritem.dataset.parsererror = e.toString();
     }
   }
+  hightlightText(sourcecode);
+}
+
+function hightlightText(sourcecode){
+  //console.log(sourcecode);
+  if(sourcecode.dataset.marks){
+    let marker=document.getElementById(sourcecode.dataset.marks);
+    //console.log(marker);
+    let content = sourcecode.value;  
+    marker.textContent='';
+    const Commands = /^(\s*)([A-Z]+:)(.*)$/;
+    for(let line of content.split('\n')){
+      let parts = line.match(Commands);
+      //console.log(line,parts);
+      if(parts){
+        marker.append(parts[1]);
+        let mark = document.createElement('SPAN');
+        mark.className='mark';
+        mark.textContent = parts[2];
+        marker.append(mark);
+        marker.append(parts[3]+'\n');
+      }else{
+        //console.log(line);
+        marker.append(line+'\n');
+      }
+    }
+    //marker.textContent = content;
+  }
+  
+  // let copy = sourcecode.cloneNode(false);
+  // copy.selectionStart = sourcecode.selectionStart;
+  // copy.selectionEnd = sourcecode.selectionEnd;
+  
+  
+  //sourcecode.replaceWith(copy);
 }
 
 function generateViews(){
-  //document.body.style.
+  var allCodes = document.querySelectorAll('.structcode');
+  for(var i=0;i<allCodes.length;i++){
+    if(allCodes[i].dataset.marks){
+      let marker = document.getElementById(allCodes[i].dataset.marks);
+      if(marker){
+        allCodes[i].addEventListener('scroll',(evt)=>{
+          //console.log(marker, evt);
+          let mstyle=getComputedStyle(marker);
+          marker.style.top = 
+              -evt.target.scrollTop
+              +parseInt(mstyle.insetInlineStart);
+        });
+      }
+    }
+  }
   var allViews = document.querySelectorAll('.structview');
   for(var i=0;i<allViews.length;i++){
     updateView(allViews[i]);
   }
+  
+  
 }
 window.addEventListener('DOMContentLoaded', generateViews);
 
